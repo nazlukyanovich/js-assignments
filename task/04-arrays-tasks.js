3d0e28151272c62891e87f35efaf4e7829a7d92c
Nazar Lukyanovich <naz.lukyanovich@gmail.com>
2020-07-08 13:42:06 +0300

    no message

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -271,7 +271,8 @@ function getSecondItems(arr) {
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
 function propagateItemsByPositionIndex(arr) {
-   return arr.map((n, index) => Array(index + 1).fill(n).flat());
+   let newArr = arr.map((n, index) => Array(index + 1).fill(n).flat());
+   return newArr;
 }
 
 
